edeedca5e9ddc5d2f75af42772b3c435d28dda9f
Daniel <dniel.rx@gmail.com>
2021-11-22 19:01:17 -0500

    Reto

---
+++ proyectoBase/src/main/java/com/usa/demo/RetoCacharreroApplication.java
@@ -0,0 +1,13 @@
+package com.usa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoCacharreroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoCacharreroApplication.class, args);
+	}
+
+}
---
+++ proyectoBase/src/test/java/com/usa/demo/RetoCacharreroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoCacharreroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
